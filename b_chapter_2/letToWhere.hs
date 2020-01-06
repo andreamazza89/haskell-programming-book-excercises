28de18d8bbc92169d5119ddc8c0dd9a4ea2e8260
andrea mazzarella <andrea@andreas-MacBook-Pro.local>
2020-01-06 09:00:40 +0100

    some initial exercises

---
+++ b_chapter_2/letToWhere.hs
@@ -0,0 +1,30 @@
+module LetToWhere where
+
+-- let x = 3; y = 1000 in x * 3 + y
+
+
+one :: Int
+one = x * 3 + y
+  where x = 3
+        y = 1000
+
+-- let y = 10; x = 10 * 5 + y in x * 5
+
+two :: Int
+two = x * 5
+  where y = 10
+        x = 10 * 5 + y
+
+
+
+-- let x = 7
+--     y = negate x
+--     z = y * 10
+-- in z / x + y
+
+three :: Float
+three = z / x + y
+  where x = 7
+        y = (-x)
+        z = y * 10
+
